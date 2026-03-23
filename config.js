2dc89e32b865a5f794ff9f44870951fc76c251ec
coupang.соm <sbkarimov@gmail.com>
2026-03-23 15:29:09 +0400

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.APP_CONFIG = {
-  supabaseUrl: https://nefzehbylviezefwxath.supabase.co,
-  supabaseAnonKey: sb_publishable_5HmNVLIpmocf2cLYODq1BQ_aChFFy-X,
+  supabaseUrl: 'https://nefzehbylviezefwxath.supabase.co',
+  supabaseAnonKey: 'sb_publishable_5HmNVLpmocf2cLYODq1BQ_aChFFy-X',
   unlockPassword: '1111'
 };
