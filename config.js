479ba14ac2d8205a1da7b88375d51d577e0699b4
coupang.соm <sbkarimov@gmail.com>
2026-03-23 15:35:30 +0400

    Change Supabase anon key in config.js
    Updated the Supabase anonymous key in the configuration.

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.APP_CONFIG = {
   supabaseUrl: 'https://nefzehbylviezefwxath.supabase.co',
-  supabaseAnonKey: 'sb_publishable_5HmNVLpmocf2cLYODq1BQ_aChFFy-X',
+  supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5lZnplaGJ5bHZpZXplZnd4YXRoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQyNjE3MDEsImV4cCI6MjA4OTgzNzcwMX0.g3DZoXxe-d3oFQorpr-l5Mf55riNotqbiWpZJRzqPKw',
   unlockPassword: '1111'
 };
